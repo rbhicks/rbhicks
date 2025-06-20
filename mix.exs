a5db0d6324901db9c4296131eddec1b3234e9996
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-06-19 20:58:56 -0700

    ingiter addition of ash postgres.

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Rbhicks.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ash_postgres, "~> 2.0"},
       {:sourceror, "~> 1.8", only: [:dev, :test]},
       {:ash_phoenix, "~> 2.0"},
       {:ash, "~> 3.0"},
@@ -75,10 +76,10 @@ defmodule Rbhicks.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "ash.setup", "assets.setup", "assets.build", "run priv/repo/seeds.exs"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      test: ["ash.setup --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind rbhicks", "esbuild rbhicks"],
       "assets.deploy": [
