ad75f5d323eae6ef3a481d49c6f507874badaaa1
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-06-20 19:29:55 -0700

    ack

--- lib/rbhicks/blog/post_tag.ex
+++ lib/rbhicks/blog/post_tag.ex
@@ -5,4 +5,19 @@ defmodule Rbhicks.Blog.PostTag do
     table "post_tags"
     repo Rbhicks.Repo
   end
+  actions do
+    defaults [:create, :read, :destroy]
+  end
+
+  relationships do
+    belongs_to :post, Rbhicks.Blog.Post do
+      primary_key? true
+      allow_nil? false
+    end
+
+    belongs_to :tag, Rbhicks.Blog.Tag do
+      primary_key? true
+      allow_nil? false
+    end
+  end
 end
