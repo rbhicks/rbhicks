b610e970f13016c703d932e7e41dc1e53f61c4be
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-06-20 19:27:35 -0700

    ack

--- lib/rbhicks/blog/post.ex
+++ lib/rbhicks/blog/post.ex
@@ -5,4 +5,33 @@ defmodule Rbhicks.Blog.Post do
     table "posts"
     repo Rbhicks.Repo
   end
+
+  actions do
+    defaults [:read]
+
+    create :create
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+      public? true
+    end
+    
+    attribute :body, :string do
+      allow_nil? false
+      public? true
+    end
+  end
+
+  # relationships do
+  #   has_many :tag_relationships, Rbhicks.Blog.PostTag
+
+  #   many_to_many :tags, Rbhicks.Blog.Tag do
+  #     join_relationship :tag_relationships
+  #     destination_attribute_on_join_resource :tag_id
+  #   end
+  # end
 end
