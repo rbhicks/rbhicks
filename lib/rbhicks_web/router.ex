615cabb0ac5460c25554c20cb412d979bc6f7db4
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-07-16 12:54:04 -0700

    added authentication

--- lib/rbhicks_web/router.ex
+++ lib/rbhicks_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RbhicksWeb.Router do
   use RbhicksWeb, :router
 
+  import RbhicksWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RbhicksWeb.Router do
     plug :put_root_layout, html: {RbhicksWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -49,5 +52,33 @@ defmodule RbhicksWeb.Router do
       live_dashboard "/dashboard", metrics: RbhicksWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", RbhicksWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{RbhicksWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", RbhicksWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{RbhicksWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
