e066ba12e5db30acb560e17acbd6f5c614127578
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-07-16 18:33:23 -0700

    incremental

--- lib/rbhicks_web/components/multi_select_component.ex
+++ lib/rbhicks_web/components/multi_select_component.ex
@@ -4,12 +4,34 @@ defmodule RbhicksWeb.MultiSelectComponent do
   use Phoenix.LiveComponent
 
   alias RbhicksWeb.CoreComponents
-  
+
+  @impl true
   def render(assigns) do
     ~H"""
     <div>
-      <CoreComponents.input type="select" name="tags" options={[:ack, :oop, :zorg]} value="" />
+      <CoreComponents.input type="select" name="tags" options={[:ack, :oop, :zorg]} value="" phx-change="tags-changed" phx-target={@myself} />
     </div>
     """
+  end
+
+  @impl true
+  def handle_event("tags-changed", params, socket) do
+    "357filj1mdzsqlg6fhbcleu272ka1b" |> IO.puts
+    "357filj1mdzsqlg6fhbcleu272ka1b" |> IO.puts
+    "357filj1mdzsqlg6fhbcleu272ka1b" |> IO.puts
+    params |> IO.inspect(limit: :infinity)
+    "==============================" |> IO.puts
+    socket |> IO.inspect(limit: :infinity)
+    "6mugwcjfp4kpqd9wd5dgzhenod93a6" |> IO.puts
+    "6mugwcjfp4kpqd9wd5dgzhenod93a6" |> IO.puts
+    "6mugwcjfp4kpqd9wd5dgzhenod93a6" |> IO.puts
+    {:noreply, socket}
+  end
+  
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:tags, [])
+    }
   end
 end
