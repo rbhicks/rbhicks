13eeea127b930e496551492ee63aadc7394a33c3
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-08-14 22:36:32 -0700

    updated phoenix version.

--- mix.exs
+++ mix.exs
@@ -35,8 +35,8 @@ defmodule Rbhicks.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.8.0-rc.4", override: true},
-      {:phoenix_ecto, "~> 4.5"},
+      {:phoenix, "~> 1.8", override: true},
+      {:phoenix_ecto, "~> 4.5}",
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
