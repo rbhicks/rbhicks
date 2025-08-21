cf15fd6b08b8569b0cf7dd1974130a3862fd75f5
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-08-20 21:43:24 -0700

    updated liveview

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Rbhicks.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.4"},
+      {:phoenix_live_view, "~> 1.1.8"},
       {:lazy_html, ">= 0.0.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.9", runtime: Mix.env() == :dev},
